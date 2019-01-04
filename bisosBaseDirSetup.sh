5883182c090acee4632754e714084e6dca33974f
Mohsen-Bisp0017 BANAN <form@mohsen.banan.1.byname.net>
2019-01-03 23:06:29 -0800

    cononicalized.

--- bisosBaseDirSetup.sh
+++ bisosBaseDirSetup.sh
@@ -5,11 +5,18 @@
 # 
 #
 
+function echoErr { echo "E: $@" 1>&2; }
+function echoAnn { echo "A: $@" 1>&2; }
+function echoOut { echo "$@"; }
+
+
 function bisosBaseDirSetup {
     local currentUser=$(id -un)
     local currentUserGroup=$(id -g -n ${currentUser})
 
-    local bisosRoot="/bisos"
+    local bisosRootDir="/bisos"
+    local bxoRootDir="/bxo"
+    local deRunRootDir="/de/run"        
 
     if [ $( type -t deactivate ) == "function" ] ; then
 	deactivate
@@ -19,12 +26,31 @@ function bisosBaseDirSetup {
 
     sudo -H pip install --no-cache-dir --upgrade virtualenv
 
-    sudo mkdir -p "${bisosRoot}"
-    sudo chown -R ${currentUser}:${currentUserGroup} "${bisosRoot}"
-
     sudo -H pip install --no-cache-dir --upgrade bisos.bx-bases
 
-    bx-bases -v 20 --baseDir="${bisosRoot}" -i pbdUpdate all
+    bx-platformInfoManage.py --bisosUserName="${currentUser}"  -i pkgInfoParsSet
+    bx-platformInfoManage.py --bisosGroupName="${currentUserGroup}"  -i pkgInfoParsSet     
+
+    bx-platformInfoManage.py --rootDir_bisos="${bisosRootDir}"  -i pkgInfoParsSet
+    bx-platformInfoManage.py --rootDir_bxo="${bxoRootDir}"  -i pkgInfoParsSet
+    bx-platformInfoManage.py --rootDir_deRun="${deRunRootDir}"  -i pkgInfoParsSet    
+
+    echoAnn "========= bx-platformInfoManage.py -i pkgInfoParsGet ========="
+    bx-platformInfoManage.py -i pkgInfoParsGet
+
+    sudo mkdir -p "${bisosRootDir}"
+    sudo chown -R ${currentUser}:${currentUserGroup} "${bisosRootDir}"
+
+    sudo mkdir -p "${bxoRootDir}"
+    sudo chown -R ${currentUser}:${currentUserGroup} "${bxoRootDir}"
+
+    sudo mkdir -p "${deRunRootDir}"
+    sudo chown -R ${currentUser}:${currentUserGroup} "${deRunRootDir}"
+    
+    #
+    # With the above rootDirs in place, bx-bases need not do any sudo-s
+    #
+    bx-bases -v 20 --baseDir="${bisosRootDir}" -i pbdUpdate all
 }
 
 bisosBaseDirSetup
--- bisosMarmeInstall.sh
+++ bisosMarmeInstall.sh
@@ -1,15 +1,22 @@
 #! /bin/bash
 
-PATH="$PATH:."
-
-bisosRoot="/bisos"
-bisosVirtEnvBase="${bisosRoot}/venv/py2-bisos-3"
-
 pkgName=unisos.marme
+bisosVenvName=py2-bisos-3
+
+#########  End-Of-Params-Specification ###########
+
+if [ $( type -t deactivate ) == "function" ] ; then
+    deactivate
+fi
+
+PATH="$PATH:."
 
 if [ ! -d "${bisosVirtEnvBase}" ] ; then
     bisosBaseDirSetup.sh
 fi
-    
+
+bisosRootDir=$( bx-platformInfoManage.py  -i pkgInfoParsGet | grep rootDir_bisos | cut -d '=' -f 2 )
+bisosVirtEnvBase="${bisosRootDir}/venv/${bisosVenvName}"
+
 bisosPkgInstall.sh "${bisosVirtEnvBase}" ${pkgName}
 
